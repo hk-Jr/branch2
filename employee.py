914a88208459b4df6b3046caec990a5ef2f392ee
kishna h k <khk803123@gmail.com>
2025-10-16 17:12:33 +0530

    this file asks for employee details.

---
+++ employee.py
@@ -0,0 +1,6 @@
+name = input("Enter employee name: ")
+emp_id = int(input("Enter employee ID: "))
+age = int(input("Enter employee age: "))
+salary = float(input("Enter employee salary: "))
+
+print(f"{name}, {emp_id}, {age}, {salary}")
