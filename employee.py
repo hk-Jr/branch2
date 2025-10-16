2fedfc337ae589b13af1c7fbc89d8a7f55e8194a
kishna h k <khk803123@gmail.com>
2025-10-16 17:21:40 +0530

    commiting through new branch

--- employee.py
+++ employee.py
@@ -2,5 +2,10 @@ name = input("Enter employee name: ")
 emp_id = int(input("Enter employee ID: "))
 age = int(input("Enter employee age: "))
 salary = float(input("Enter employee salary: "))
+exp= int (input("enter the experience in years:"))
 
 print(f"{name}, {emp_id}, {age}, {salary}")
+
+#new commit through other branch
+print(f"\n\n{exp}")
+
